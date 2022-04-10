46bf48bab7cb0952cc1b7077c7101ce2ad500c27
shashwatraj <er.shashwatraj@gmail.com>
2022-04-10 22:44:21 +0530

    insert updated

--- js DSA/Trees/BSTinsert.js
+++ js DSA/Trees/BSTinsert.js
@@ -1,44 +1,45 @@
 class Node{
-	constructor(value){
-
-	        this.value = vslue;
-	        this.left = null;
-	        this.right = null;
-	}
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
-
 class BinarySearchTree{
-	constructor(){
-	        this.root = null;
-	}
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root===null){
+            this.root = newNode;
+            return this;
+        }else{
+          var current = this.root;
+        while(true){
+            if(value<current.value){
+                if(current.left===null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }else if(value>current.value){
+                if(current.right===null){
+                    current.right = newNode;
+                    return this;
+                }else{
+                    current = current.right;
+                }
+                
+             }
+          }
 
-	insert(value){
-	      var newNode = new Node(value);
-	      if(this.root===null){
-	          this.root = newNode;
-	          return this;
-	      }
-
-	      var current = this.root;
-	      while(true){
-	      if(value===current.value)return undefined;
-	      if(value<current.value){
-	      if(current.left=null){
-	      current.left=newNode;
-	      return this;
-	      }
-	        current = current.left
-
-	      } else{
-
-	      	if(current.right===null){
-	      		current.right = newNode;
-	      		return this;
-
-	      	}
-
-	      	current = current.right;
-	      }
-	      }
-	}
+        }
+    }
 }
+
+var tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
