f8974d5e56508208d6a6c6a29001e946f2b186eb
shashwatraj <er.shashwatraj@gmail.com>
2022-04-18 01:38:20 +0530

    DFS DSA JS

--- js DSA/Trees/DFSpostorder.js
+++ js DSA/Trees/DFSpostorder.js
@@ -95,3 +95,12 @@ class BinarySearchTree{
         }
     }
 }
+
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
