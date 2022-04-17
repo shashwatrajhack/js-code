0be19790a837d3c99c1aa4ca278e0f5bd99fca43
shashwatraj <er.shashwatraj@gmail.com>
2022-04-17 23:52:22 +0530

    DFS.js

--- js DSA/Trees/TreeTraversal.js
+++ js DSA/Trees/TreeTraversal.js
@@ -1,4 +1,37 @@
-BFS(){
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root===null){
+            this.root = newNode;
+            return this;
+        }else{
+          var current = this.root;
+        while(true){
+            if(value<current.value){
+                if(current.left===null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            }else if(value>current.value){
+                if(current.right===null){
+                    current.right = newNode;
+                    return this;
+                }else{
+                    current = current.right;
+                }
+
+                BFS(){
     var node = this.root;
     data = [];
     queue = [];
@@ -11,6 +44,14 @@ BFS(){
     }
     return data;
 }
+             }
+          }
+
+        }
+    }
+}
+
+
 
 var tree = new BinarySearchTree();
 tree.insert(10);
