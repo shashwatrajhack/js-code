ba89e8dc709bb5d7c1024917be352c5116f01f6d
shashwatrajhack <er.shashwatraj@gmail.com>
2023-08-24 14:01:50 +0530

    Basic operation of graph data structure

---
+++ js DSA/Graph/addvrtx.js
@@ -0,0 +1,17 @@
+class graph{
+    constructor(){
+        this.adjacencyList={};
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]; 
+        
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+}
+
+var g = new graph();
