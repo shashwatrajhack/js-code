9acc1df9b4ecb1c56fa4320a7b98cd205cc29c5e
shashwatrajhack <er.shashwatraj@gmail.com>
2024-05-16 12:06:54 +0530

    JS promises

--- leetcode question/p.js
+++ leetcode question/p.js
@@ -31,4 +31,41 @@ function hello(){
     console.log("hello");
 }
 
-setTimeout(hello,2000);//time out
+setTimeout(hello);//time out
+
+//callback --> function called in another function is called callback;
+
+function sum(a,b){
+    console.log(a+b)
+}
+
+function calculator(a,b,sumCallback){
+    sumCallback(a,b);
+
+}
+
+calculator(1,4,sum)
+
+
+//Promises is an object oin js which has 3 state pending,reject and resolve.
+
+// let promise = new Promise((resolve,reject) => {
+//     console.log("I am a promises");
+//     //resolve("successful");
+//     reject("some error occured");
+
+// });
+
+//how to use promises --> by using .then and .catch method;
+
+const getPromise = () => {
+    return new promise((resolve,reject) => {
+        console.log("i am a promise");
+        resolve("success");
+    })
+}
+
+let promise = getPromise();
+promise.then((res) => {
+    console.log("promise fulfilled",res);
+})
